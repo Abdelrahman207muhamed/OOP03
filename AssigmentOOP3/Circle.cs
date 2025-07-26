deb53f81eec247fbf5a98b27f828f932f22dc2e9
user <user@DESKTOP-0FTCJBS>
2025-07-26 20:44:00 +0300

    Assignment 03 OOP

---
+++ AssigmentOOP3/Circle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssigmentOOP3
+{
+    internal class Circle:ICircle
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public double Radius => radius;
+
+        public double Area => Math.PI * radius * radius;
+
+        double ICircle.Radius { get ; set ; }
+        double IShape.Area { get ; set ; }
+
+        public void DisplayShapeInfo()
+        {
+            Console.WriteLine($"Circle: Radius = {Radius:F2}, Area = {Area:F2}");
+        }
+    }
+}
---
+++ AssigmentOOP3/Rectangle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssigmentOOP3
+{
+    internal class Rectangle : IRectangle
+    {
+        private double width;
+        private double height;
+
+        public Rectangle(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public double Width => width;
+
+        public double Height => height;
+
+        public double Area => width * height;
+
+        double IRectangle.Width { get ; set ; }
+        double IRectangle.Height { get ; set ; }
+        double IShape.Area { get; set; }
+
+        public void DisplayShapeInfo()
+        {
+            Console.WriteLine($"Rectangle: Width = {Width:F2}, Height = {Height:F2}, Area = {Area:F2}");
+        }
+
+    }
+}
